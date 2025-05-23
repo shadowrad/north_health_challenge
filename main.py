a1b18d5b2c74bc91aabecc0ba4470c1dbbc8310a
Rodrigo Duran <duranrodrigo@gmail.com>
2025-05-23 10:14:22 -0300

    Add: simple printer to show the result

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from models.pair_finder import PairFinder
 def print_pairs_by_sum(arr: list[int]) -> None:
     find_pairs = PairFinder(arr)
     result = find_pairs.find_pairs_with_equal_sum(2)
+    result = dict(sorted(result.items()))
     for total, pairs in result.items():
         formatted_pairs = " ".join(f"({a}, {b})" for a, b in pairs)
         print(f"Pairs : {formatted_pairs} have sum : {total}")
